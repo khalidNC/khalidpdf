b17b378f0ed9020ef826d8d146426d502825fdc7
KhalidNC <hussainkhalid70@yahoo.com>
2023-11-18 18:58:55 +0600

    Updated pdf2text.py

--- khalidpdf/pdf2text.py
+++ khalidpdf/pdf2text.py
@@ -1,2 +1,13 @@
-def convert():
+""" This module provides function to convert a PDF to text. """
+
+def convert(self, path):
+  """ 
+  Convert the given PDF to text.
+  
+  Parameters:
+  path (str): A path to a PDF file
+
+  Returns: 
+  Str: Then content of the PDF file as text 
+  """
   print("pdf2text")
